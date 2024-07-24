5c11a958c841ab23ff1913f2d068b79397ed268a
Zubair Sheikh <zubair@xertk.com>
2024-07-24 23:32:22 +0500

    fix: test issue

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -6,16 +6,16 @@ from .views import (
 )
 
 urlpatterns = [
-    path("register", RegisterView.as_view(), name="register"),
-    path("login", LoginView.as_view(), name="login"),
-    path("logout", LogoutView.as_view(), name="logout"),
-    path("refresh-tokens", RefreshTokenView.as_view(), name="refresh-tokens"),
-    path("forgot-password", ForgotPasswordView.as_view(), name="forgot-password"),
-    path("reset-password", ResetPasswordView.as_view(), name="reset-password"),
+    path("auth/register", RegisterView.as_view(), name="register"),
+    path("auth/login", LoginView.as_view(), name="login"),
+    path("auth/logout", LogoutView.as_view(), name="logout"),
+    path("auth/refresh-tokens", RefreshTokenView.as_view(), name="refresh-tokens"),
+    path("auth/forgot-password", ForgotPasswordView.as_view(), name="forgot-password"),
+    path("auth/reset-password", ResetPasswordView.as_view(), name="reset-password"),
     path(
-        "send-verification-email",
+        "auth/send-verification-email",
         SendVerificationEmailView.as_view(),
         name="send-verification-email",
     ),
-    path("verify-email", VerifyEmailView.as_view(), name="verify-email"),
+    path("auth/verify-email", VerifyEmailView.as_view(), name="verify-email"),
 ]
--- core/project/urls.py
+++ core/project/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-API_PREFIX = "api/"
+API_PREFIX = "v1/"
 
 urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path("admin/", admin.site.urls),
+    path(API_PREFIX, include('core.accounts.urls')),
 ]
 
 if settings.DEBUG:
